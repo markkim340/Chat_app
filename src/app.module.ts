09316b8d6db69578950e612d62e441ac4bbd5256
Mark Kim <mark340@naver.com>
2023-02-27 16:23:48 +0900

    Doker_Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+import { LoggerModule } from './logger/logger.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig, databaseConfig] }),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
