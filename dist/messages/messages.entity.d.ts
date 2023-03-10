ec6d1c3851ba32e736919e478a60e5120b930ab7
Mark Kim <mark340@naver.com>
2023-03-10 18:10:58 +0900

    refactor

--- dist/messages/messages.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Rooms } from 'src/chat/Rooms.entity';
-export declare class Messages {
-    id: number;
-    content: string;
-    createdAt: Date;
-    updatedAt: Date;
-    room: Rooms;
-}
